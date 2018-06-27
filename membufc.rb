5865b200a181e22cd6725147878542c4c6ac06e7
Tal Kol <talkol@gmail.com>
2018-06-28 01:15:21 +0300

    Update membufc.rb

--- membufc.rb
+++ membufc.rb
@@ -20,8 +20,9 @@ class Membufc < Formula
     cd bin_path do
       # Install the compiled binary into Homebrew's `bin` - a pre-existing
       # global variable
-      system "go", "get", "-u", "github.com/gobuffalo/packr/..."
-      system buildpath/"bin/packr", "build", "-o", bin/"membufc", "."
+      #system "go", "get", "-u", "github.com/gobuffalo/packr/..."
+      #system buildpath/"bin/packr", "build", "-o", bin/"membufc", "."
+      system "go", "build", "-o", bin/"membufc", "."
     end
   end
 
