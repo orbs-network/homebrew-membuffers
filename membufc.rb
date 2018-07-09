a76c0ae52690714364c733a7768db6cc1e752890
Tal Kol <talkol@gmail.com>
2018-07-09 08:19:17 +0300

    Update membufc.rb

--- membufc.rb
+++ membufc.rb
@@ -3,8 +3,8 @@ class Membufc < Formula
   homepage "https://github.com/orbs-network/membuffers"
 
   # Source code archive. Each tagged release will have one
-  url "https://github.com/orbs-network/membuffers/archive/0.0.11.tar.gz"
-  sha256 "f8f87a960e430dd76b405b53d3279ff8aa8a27f506d3bf546caad81fef4e3140"
+  url "https://github.com/orbs-network/membuffers/archive/0.0.12.tar.gz"
+  sha256 "aad2d93ceecbe7abb64083e2053edb2a88edbc862dc97183ba9de78c39f53a1e"
   head "https://github.com/orbs-network/membuffers"
   
   depends_on "go" => :build
@@ -29,6 +29,6 @@ class Membufc < Formula
   test do
     # "2>&1" redirects standard error to stdout. The "2" at the end means "the
     # exit code should be 2".
-    assert_match "membufc 0.0.11", shell_output("#{bin}/membufc --version 2>&1", 2)
+    assert_match "membufc 0.0.12", shell_output("#{bin}/membufc --version 2>&1", 2)
   end
 end
